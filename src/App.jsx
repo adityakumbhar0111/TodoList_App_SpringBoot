b8e00dbfb1752f12260c0ec7c03c3dcd67440302
adityakumbhar0111 <adityakumbhar0111@gmail.com>
2026-03-14 14:15:45 +0530

    TodoList App React Frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserTask from "./UserTask";
+
+function App() {
+  return (
+    <>
+      <UserTask />
+    </>
+  );
+}
+
+export default App;
---
+++ src/UserTask.jsx
@@ -0,0 +1,187 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import "./App.css";
+
+const UserTask = () => {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
+
+  const [editId, setEditId] = useState(null);
+  const [msg, setMsg] = useState("Response");
+
+  useEffect(() => {
+    getAllTask();
+  }, []);
+
+  const getAllTask = () => {
+    axios
+      .get("http://localhost:8080/get-task")
+      .then((res) => {
+        setTasks(res.data);
+        console.table(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // ADD TASK
+  const addUserTask = (e) => {
+    e.preventDefault();
+
+    const info = {
+      task: task,
+      date: date,
+    };
+
+    axios
+      .post("http://localhost:8080/add-task", info)
+      .then((res) => {
+        setMsg(res.data);
+        getAllTask();
+        setTask("");
+        setDate("");
+      })
+      .catch((err) => setMsg(err.message));
+  };
+
+  // DELETE TASK
+  const deleteUserTask = (id) => {
+    axios
+      .delete(`http://localhost:8080/delete-task/${id}`)
+      .then((res) => {
+        setMsg(res.data);
+        getAllTask();
+      })
+      .catch((err) => setMsg(err.message));
+  };
+
+  // FILL FORM FOR UPDATE
+  const editTask = (t) => {
+    setTask(t.task);
+    setDate(t.date);
+    setEditId(t.id);
+  };
+
+  // UPDATE TASK
+  const updateUserTask = (e) => {
+    e.preventDefault();
+
+    const info = {
+      task: task,
+      date: date,
+    };
+
+    axios
+      .put(`http://localhost:8080/update-task/${editId}`, info)
+      .then((res) => {
+        setMsg(res.data);
+        getAllTask();
+        setTask("");
+        setDate("");
+        setEditId(null);
+      })
+      .catch((err) => setMsg(err.message));
+  };
+
+  // UPDATE STATUS
+  const updateUserTaskStatus = (id) => {
+    let msg = "Completed";
+
+    axios
+      .patch(`http://localhost:8080/update-status/${id}/${msg}`)
+      .then((res) => {
+        setMsg(res.data);
+
+        // update local state
+        // setTasks((prevTasks) =>
+        //   prevTasks.map((t) => (t.id === id ? {...t, status: "Completed"} : t)),
+        // );
+        getAllTask();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="mainDiv">
+      <h2 className="todo-title">📝 Todo List</h2>
+
+      {msg !== "Response" && (
+        <div className="popup">
+          <p>{msg}</p>
+          <button onClick={() => setMsg("Response")}>OK</button>
+        </div>
+      )}
+
+      <form onSubmit={editId ? updateUserTask : addUserTask}>
+        <input
+          className="taskInput"
+          type="text"
+          placeholder="Enter Task..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          required
+        />
+
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
+        />
+
+        <button type="submit">{editId ? "Update Task" : "Add Task"}</button>
+      </form>
+
+      <hr />
+
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Task</th>
+            <th>Date</th>
+            <th>Update</th>
+            <th>Delete</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {tasks.map((t) => (
+            <tr
+              key={t.id}
+              className={t.status === "Completed" ? "completed" : "incomplete"}
+            >
+              <td>{t.id}</td>
+              <td>{t.task}</td>
+              <td>{new Date(t.date).toLocaleDateString()}</td>
+
+              <td>
+                <button onClick={() => editTask(t)} className="update">
+                  Update
+                </button>
+              </td>
+
+              <td>
+                <button onClick={() => deleteUserTask(t.id)} className="delete">
+                  Delete
+                </button>
+              </td>
+
+              <td>
+                <button
+                  onClick={() => updateUserTaskStatus(t.id)}
+                  className="update"
+                >
+                  Completed
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserTask;
